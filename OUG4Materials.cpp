ba3a5ba18112274b3d2c3a46954a687981964c19
Justin Warren <jw402216@ohio.edu>
2024-09-20 14:10:28 -0400

    Update OUG4Materials.cpp

--- OUG4Materials.cpp
+++ OUG4Materials.cpp
@@ -23,7 +23,11 @@ OUG4Materials::OUG4Materials(G4NistManager* thenist)
 // Destructor
 OUG4Materials::~OUG4Materials()
 {
-
+    // delete MuMetal_UNS14080;
+    // delete HAVAR;
+    // delete C7LYC_99;
+    // delete C6LYC_95;
+    // delete Quartz;
 }
 
 // Set the G4NistManager
@@ -77,13 +81,11 @@ void OUG4Materials::SetC6LYCMaterial_95()
     G4Element* elCs = NistManager->FindOrBuildElement(55);
 
     // Define the CLYC molecule
-    G4Material* CLYC_molecule = new G4Material(name,density,ncomponents);
-    CLYC_molecule->AddElement(elCs, 20*perCent);
-    CLYC_molecule->AddElement(Li6Enh_el, 10*perCent);
-    CLYC_molecule->AddElement(elY, 10*perCent);
-    CLYC_molecule->AddElement(elCl, 60*perCent);
-
-    C6LYC_95 = CLYC_molecule;
+    C6LYC_95 = new G4Material(name,density,ncomponents);
+    C6LYC_95->AddElement(elCs, 20*perCent);
+    C6LYC_95->AddElement(Li6Enh_el, 10*perCent);
+    C6LYC_95->AddElement(elY, 10*perCent);
+    C6LYC_95->AddElement(elCl, 60*perCent);
 
     return;
 
@@ -121,13 +123,11 @@ void OUG4Materials::SetC7LYCMaterial_99()
     G4Element* elCs = NistManager->FindOrBuildElement(55);
 
     // Define the clyc molecule and add the components to it
-    G4Material* CLYC_molecule = new G4Material(name,density,ncomponents);
-    CLYC_molecule->AddElement(elCs, 20*perCent);
-    CLYC_molecule->AddElement(Li7Enh_el, 10*perCent);
-    CLYC_molecule->AddElement(elY, 10*perCent);
-    CLYC_molecule->AddElement(elCl, 60*perCent);
-
-    C7LYC_99 = CLYC_molecule;
+    C7LYC_99 = new G4Material(name,density,ncomponents);
+    C7LYC_99->AddElement(elCs, 20*perCent);
+    C7LYC_99->AddElement(Li7Enh_el, 10*perCent);
+    C7LYC_99->AddElement(elY, 10*perCent);
+    C7LYC_99->AddElement(elCl, 60*perCent);
 
     return;
 }
@@ -151,17 +151,15 @@ void OUG4Materials::SetHAVARMaterial()
     G4Element* elFe = NistManager->FindOrBuildElement(26);
 
     // Define the new material and add the components to it
-    G4Material* HAVAR_alloy = new G4Material(name,density,ncomponents);
-    HAVAR_alloy->AddElement(elCo, 42*perCent);
-    HAVAR_alloy->AddElement(elCr, 19.5*perCent);
-    HAVAR_alloy->AddElement(elNi, 12.7*perCent);
-    HAVAR_alloy->AddElement(elW, 2.7*perCent);
-    HAVAR_alloy->AddElement(elMo, 2.2*perCent);
-    HAVAR_alloy->AddElement(elMn, 1.6*perCent);
-    HAVAR_alloy->AddElement(elC, 0.2*perCent);
-    HAVAR_alloy->AddElement(elFe, 19.1*perCent);
-
-    HAVAR = HAVAR_alloy;
+    HAVAR = new G4Material(name,density,ncomponents);
+    HAVAR->AddElement(elCo, 42*perCent);
+    HAVAR->AddElement(elCr, 19.5*perCent);
+    HAVAR->AddElement(elNi, 12.7*perCent);
+    HAVAR->AddElement(elW, 2.7*perCent);
+    HAVAR->AddElement(elMo, 2.2*perCent);
+    HAVAR->AddElement(elMn, 1.6*perCent);
+    HAVAR->AddElement(elC, 0.2*perCent);
+    HAVAR->AddElement(elFe, 19.1*perCent);
 
     return;
 }
@@ -180,12 +178,10 @@ void OUG4Materials::Set_MuMetal_UNS14080Material()
     G4Element* elFe = NistManager->FindOrBuildElement(26);
 
     // Define the new material and add the components to it
-    G4Material* MuMetal_alloy = new G4Material(name,density,ncomponents);
-    MuMetal_alloy->AddElement(elNi, 80*perCent);
-    MuMetal_alloy->AddElement(elMo, 6*perCent);
-    MuMetal_alloy->AddElement(elFe, 14*perCent);
-    
-    MuMetal_UNS14080 = MuMetal_alloy;
+    MuMetal_UNS14080 = new G4Material(name,density,ncomponents);
+    MuMetal_UNS14080->AddElement(elNi, 80*perCent);
+    MuMetal_UNS14080->AddElement(elMo, 6*perCent);
+    MuMetal_UNS14080->AddElement(elFe, 14*perCent);
 
     return;
 }
@@ -203,11 +199,9 @@ void OUG4Materials::SetQuartzMaterial()
     G4Element* elO = NistManager->FindOrBuildElement(8);
 
     // Define the new material and add the components to it
-    G4Material* Quartz_molecule = new G4Material(name="Quartz",density,ncomponents=2);
-    Quartz_molecule->AddElementByNumberOfAtoms(elSi,1);
-    Quartz_molecule->AddElementByNumberOfAtoms(elO,2);
-
-    Quartz = Quartz_molecule;
+    Quartz = new G4Material(name="Quartz",density,ncomponents=2);
+    Quartz->AddElementByNumberOfAtoms(elSi,1);
+    Quartz->AddElementByNumberOfAtoms(elO,2);
 
     return;
 }
